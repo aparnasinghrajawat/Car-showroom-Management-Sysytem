6730de66242ebde85fbd29ef0e42a0ecf92e7f4b
aparna singh rajawat <135022173+aparnasinghrajawat@users.noreply.github.com>
2024-04-13 14:47:06 +0530

    UTILITY.java

---
+++ UTILITY.java
@@ -0,0 +1,18 @@
+
+  // IntelliJ API Decompiler stub source generated from a class file
+  // Implementation of methods is not available
+
+public class Cars extends Showroom implements utility {
+    java.lang.String car_name;
+    java.lang.String car_color;
+    java.lang.String car_fuel_type;
+    int car_price;
+    java.lang.String car_type;
+    java.lang.String car_transmission;
+
+    public Cars() { /* compiled code */ }
+
+    public void get_deatils() { /* compiled code */ }
+
+    public void set_details() { /* compiled code */ }
+}
